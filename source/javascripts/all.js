53e546c55428e488515ec70723828bec43cbc11c
Will <wrussell35@gmail.com>
2014-11-18 17:26:52 -0500

    navbar ajax request working

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,3 +3,23 @@
 
 //= require app
 //= require script
+
+$(function() {
+    $('#location-button').on('click', function () {
+        var input_address = $('#address').val();
+        $.ajax({
+        type: "GET",
+        url: "http://requestb.in/t9jg5xt9",
+        data: { address : input_address },
+        });
+    });
+});
+
+// $(".legislators").append(legislator);
+
+
+
+
+        // success: function(data) {
+        // var legislator =$.map(data,
+        // function (legislator) {return "legislator.first_name";});
