59ca9bcc86afff0f9d1e3d29e827c1529e2c04f8
Will <wrussell35@gmail.com>
2014-11-19 12:33:46 -0500

    ajax request working

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -9,44 +9,17 @@ $(function() {
         var input_address = $('#address').val();
         $.ajax({
         type: "GET",
-        url: "http://requestb.in/t9jg5xt9",
+        url: "https://peaceful-sea-4129.herokuapp.com/api/v1/search.json",
         data: { address : input_address },
+        success: function(data) {
+            var profile = data;
+            $(".legislators").append(JSON.stringify(profile["legislators"][0]["firstname"])) +
+            $(".legislators").append(JSON.stringify(profile["legislators"][0]["lastname"]));
+            }
         });
     });
 });
 
 
-// $(".legislators").append(legislator);
 
 
-
-
-        // success: function(data) {
-        // var legislator =$.map(data,
-        // function (legislator) {return "legislator.first_name";});
-
-
-
-$.ajax({
-  dataType: "json",
-  url: "https://peaceful-sea-4129.herokuapp.com/api/v1/search.json",
-  data: {},
-  success: function(data) {
-    var profile = data;
-
-    $(".legislators").append(profile[0].first_name)
-  },
-});
-
-
-
-$.ajax({
-  dataType: "json",
-  url: "https://api.github.com/users/willrussell",
-  data: {},
-  success: function(data) {
-    var profile = data;
-
-    $(".followers").append(profile.followers)
-  },
-});
