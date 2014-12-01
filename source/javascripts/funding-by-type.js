2d84e00bee06a19f2e1d24b938f34c34d455b423
Will <wrussell35@gmail.com>
2014-12-01 18:17:33 -0500

    javascript broken out into separate files

---
+++ source/javascripts/funding-by-type.js
@@ -0,0 +1,106 @@
+//--- Highcharts for section 2 graph ---
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'area',
+            spacingBottom: 30,
+                        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || 'transparent'
+
+
+        },
+        title: {
+            text: 'Timeline w/missing values *'
+        },
+        subtitle: {
+            text: '* Jane\'s banana consumption is unknown',
+            floating: true,
+            align: 'right',
+            verticalAlign: 'bottom',
+            y: 15
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'left',
+            verticalAlign: 'top',
+            x: 150,
+            y: 100,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+        },
+        xAxis: {
+            categories: ['Apples', 'Pears', 'Oranges', 'Bananas', 'Grapes', 'Plums', 'Strawberries', 'Raspberries']
+        },
+        yAxis: {
+            title: {
+                text: 'Y-Axis'
+            },
+            labels: {
+                formatter: function () {
+                    return this.value;
+                }
+            }
+        },
+        tooltip: {
+            formatter: function () {
+                return '<b>' + this.series.name + '</b><br/>' +
+                    this.x + ': ' + this.y;
+            }
+        },
+        plotOptions: {
+            area: {
+                fillOpacity: 0.5
+            }
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: 'John',
+            data: [0, 1, 4, 4, 5, 2, 3, 7]
+        }, {
+            name: 'Jane',
+            data: [1, 0, 3, null, 3, 1, 2, 1]
+        }]
+    });
+});
+
+// Highcharts js graph for section 2 pt. 2
+$(function () {
+    $('#slider-two').highcharts({
+        chart: {
+            type: 'pie',
+            backgroundColor: 'transparent',
+            options3d: {
+                enabled: true,
+                alpha: 45
+            }
+        },
+        title: {
+            text: 'Funding by Type'
+        },
+        subtitle: {
+            text: '3D donut in Highcharts'
+        },
+        plotOptions: {
+            pie: {
+                innerSize: 100,
+                depth: 45
+            }
+        },
+        series: [{
+            name: 'Delivered amount',
+            data: [
+                ['Bananas', 8],
+                ['Kiwi', 3],
+                ['Mixed nuts', 1],
+                ['Oranges', 6],
+                ['Apples', 8],
+                ['Pears', 4],
+                ['Clementines', 4],
+                ['Reddish (bag)', 1],
+                ['Grapes (bunch)', 1]
+            ]
+        }]
+    });
+});
