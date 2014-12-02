6abcd914b7594a883d13d943e8baa0f9097cee77
Will <wrussell35@gmail.com>
2014-12-02 00:12:46 -0500

    funding-by-type chart complete

--- source/javascripts/funding-by-type.js
+++ source/javascripts/funding-by-type.js
@@ -1,79 +1,16 @@
 //--- Highcharts for section 2 graph ---
-$(function () {
-    $('#container').highcharts({
-        chart: {
-            type: 'area',
-            spacingBottom: 30,
-                        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || 'transparent'
-
-
-        },
-        title: {
-            text: 'Timeline w/missing values *'
-        },
-        subtitle: {
-            text: '* Jane\'s banana consumption is unknown',
-            floating: true,
-            align: 'right',
-            verticalAlign: 'bottom',
-            y: 15
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'left',
-            verticalAlign: 'top',
-            x: 150,
-            y: 100,
-            floating: true,
-            borderWidth: 1,
-            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
-        },
-        xAxis: {
-            categories: ['Apples', 'Pears', 'Oranges', 'Bananas', 'Grapes', 'Plums', 'Strawberries', 'Raspberries']
-        },
-        yAxis: {
-            title: {
-                text: 'Y-Axis'
-            },
-            labels: {
-                formatter: function () {
-                    return this.value;
-                }
-            }
-        },
-        tooltip: {
-            formatter: function () {
-                return '<b>' + this.series.name + '</b><br/>' +
-                    this.x + ': ' + this.y;
-            }
-        },
-        plotOptions: {
-            area: {
-                fillOpacity: 0.5
-            }
-        },
-        credits: {
-            enabled: false
-        },
-        series: [{
-            name: 'John',
-            data: [0, 1, 4, 4, 5, 2, 3, 7]
-        }, {
-            name: 'Jane',
-            data: [1, 0, 3, null, 3, 1, 2, 1]
-        }]
-    });
-});
-
-// Highcharts js graph for section 2 pt. 2
-$(function () {
+function type_function (type_data){
+    var total_indiv_contributions = type_data["legislators"][0]["contributors_by_type"]["Individuals"][1].slice(1, -3);
+    var total_pac_contributions = type_data["legislators"][0]["contributors_by_type"]["PACs"][1].slice(1, -3);
+    var total_indiv_contributions = parseInt(total_indiv_contributions);
+    var total_pac_contributions = parseInt(total_pac_contributions);
     $('#slider-two').highcharts({
         chart: {
             type: 'pie',
             backgroundColor: 'transparent',
             options3d: {
-                enabled: true,
-                alpha: 45
+            enabled: true,
+            alpha: 45
             }
         },
         title: {
@@ -89,18 +26,11 @@ $(function () {
             }
         },
         series: [{
-            name: 'Delivered amount',
+            name: 'Contribution',
             data: [
-                ['Bananas', 8],
-                ['Kiwi', 3],
-                ['Mixed nuts', 1],
-                ['Oranges', 6],
-                ['Apples', 8],
-                ['Pears', 4],
-                ['Clementines', 4],
-                ['Reddish (bag)', 1],
-                ['Grapes (bunch)', 1]
+                ['Individual contributions', total_indiv_contributions],
+                ['Total PAC contributions', total_pac_contributions]
             ]
-        }]
+        }],
     });
-});
+};
