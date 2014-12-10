92d3e2e48dd4a09dd47662486a70584a83c377f4
Will <wrussell35@gmail.com>
2014-12-10 16:02:05 -0500

    datatable resize

--- source/javascripts/industry-score-scatter.js
+++ source/javascripts/industry-score-scatter.js
@@ -19,7 +19,7 @@ function scatterplot_function (bigDataArray){
             },
             yAxis: {
                 title: {
-                    text: 'Likelihood of Agreement'
+                    text: 'Agreement Percentage'
                 },
                 min: 0,
                 max: 1,
